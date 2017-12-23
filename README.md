243beaec14908d539a883fa16a08adf5258dbdef
Nikita <shockck84@gmail.com>
2017-12-23 21:40:12 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CLAF
+## Clean framework for building Garry's Mod addons
+
+CLAF is a set of wrappers for standard API which makes the Garry's Mod addons development easy.  
+See [wiki](https://github.com/javabird25/gmod-claf/wiki) for the documentation.
---
+++ lua/autorun/shared/lib.lua
@@ -0,0 +1,18 @@
+--Free ID for automatic hook name
+CLAF_hookAutoNameID = 1
+
+--Throws an error if the passed argument is nil.
+function errorIfNil(any)
+    if any == nil then
+        error(any .. " shouldn't be nil")
+end
+
+--Quick hook creation function.
+function Hook(event, callback, ID)
+    if ID == nil then
+        ID = "CLAF_" .. CLAF_hookAutoNameID
+        CLAF_hookAutoNameID = CLAF_hookAutoNameID + 1
+    end
+
+    hook.Add(event, ID, callback)
+end
