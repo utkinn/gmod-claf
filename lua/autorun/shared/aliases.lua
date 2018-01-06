d08eb47a1fd2000b064a82f1571e07411f8f8d9e
Nikita <shockck84@gmail.com>
2018-01-06 18:53:33 +0500

    Add Player:IsAlive() and Player:GetArmor()

--- lua/autorun/shared/aliases.lua
+++ lua/autorun/shared/aliases.lua
@@ -2,7 +2,20 @@
 -- Some standard functions have bad names, so aliases are created for them.
 
 local entityMetaTable = FindMetaTable('Entity')
+
 -- Alias for Entity:Health().
 function entityMetaTable:GetHealth()
     return self:Health()
+end
+
+local playerMetaTable = FindMetaTable('Player')
+
+-- Alias for Player:Alive().
+function playerMetaTable:IsAlive()
+    return self:Alive()
+end
+
+-- Alias for Player:Armor().
+function playerMetaTable:GetArmor()
+    return self:Armor()
 end
