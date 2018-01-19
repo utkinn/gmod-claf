2821794477008040310b383f342198f1d96b6934
Nikita <shockck84@gmail.com>
2018-01-19 11:56:35 +0500

    Move sources from lua/autorun/shared/ to lua/shared/

--- lua/shared/lib.lua
+++ lua/shared/lib.lua
@@ -1,6 +1,6 @@
 -- Some utility functions.
 
---Free ID for automatic hook name
+-- Free ID for automatic hook name.
 local CLAF_hookAutoNameID = 1
 
 -- Always creates an error 'not implemented'.
@@ -8,15 +8,15 @@ function TODO()
     error('not implemented')
 end
 
---Throws an error if the passed argument is nil.
+-- Throws an error if the passed argument is nil.
 function ErrorIfNil(any)
     if any == nil then error('nil is not allowed here') end
 end
 
---Quick hook creation function.
+-- Quick hook creation function.
 function Hook(event, callback, ID)
     if ID == nil then
-        ID = 'CLAFHook_' .. CLAF_hookAutoNameID
+        ID = 'CLAFHook_'..CLAF_hookAutoNameID
         CLAF_hookAutoNameID = CLAF_hookAutoNameID + 1
     end
 
