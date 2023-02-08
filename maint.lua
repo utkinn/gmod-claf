6b21c8558b196c3aac38ed42865cc4f201f9c753
javabird25 <shockck84@gmail.com>
2023-02-08 11:22:40 +0500

    Primitive maintenance script

---
+++ maint.lua
@@ -0,0 +1,37 @@
+#!/usr/bin/env lua
+
+local function detectOs()
+    return package.config:sub(1, 1) == "\\" and "win" or "unix"
+end
+
+local rmF = detectOs() == "win" and "del /q" or "rm -f"
+
+local tasks = {
+    test = "busted",
+    cover = { rmF .. " luacov.*", "busted -c", "luacov" },
+    lint = "luacheck .",
+    ci = { children = { "cover", "lint" } }
+}
+
+local function runTask(taskName)
+    local taskDef = tasks[taskName]
+    if not taskDef then
+        error("Unknown taskName: " .. taskName)
+    end
+    if type(taskDef) == "table" then
+        if type(taskDef.children) == "table" then
+            for _, child in ipairs(taskDef.children) do
+                runTask(child)
+            end
+        end
+        for _, cmd in ipairs(taskDef) do
+            os.execute(cmd)
+        end
+    else
+        os.execute(taskDef)
+    end
+end
+
+for _, taskToRun in ipairs(arg) do
+    runTask(taskToRun)
+end
