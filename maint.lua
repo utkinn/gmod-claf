205d794603d237421b61eae54ad94e865925ddb8
javabird25 <shockck84@gmail.com>
2023-02-08 12:14:57 +0500

    maint.lua: exit with status code 1 on task fail

--- maint.lua
+++ maint.lua
@@ -13,10 +13,12 @@ local tasks = {
     ci = { children = { "test", "lint" } }
 }
 
+local statusCodes = {}
+
 local function runTask(taskName)
     local taskDef = tasks[taskName]
     if not taskDef then
-        error("Unknown taskName: " .. taskName)
+        error("Unknown task: " .. taskName)
     end
     if type(taskDef) == "table" then
         if type(taskDef.children) == "table" then
@@ -25,13 +27,31 @@ local function runTask(taskName)
             end
         end
         for _, cmd in ipairs(taskDef) do
-            os.execute(cmd)
+            local _, _, status = os.execute(cmd)
+            statusCodes[cmd] = status
         end
     else
-        os.execute(taskDef)
+        local _, _, status = os.execute(taskDef)
+        statusCodes[taskDef] = status
     end
 end
 
 for _, taskToRun in ipairs(arg) do
     runTask(taskToRun)
+end
+
+local redColorStart = '\x1b[31m\x1b[1m'
+local redColorEnd = '\x1b[0m'
+
+local fail = false
+print('\n')
+for cmd, status in pairs(statusCodes) do
+    if status ~= 0 then
+        print(redColorStart .. 'Command "' .. cmd .. '" failed with status ' .. status .. '.' .. redColorEnd)
+        fail = true
+    end
+end
+
+if fail then
+    os.exit(1)
 end
