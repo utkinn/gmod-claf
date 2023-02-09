934f0cf816e0ee2b77fc4050b1361355031cb514
javabird25 <shockck84@gmail.com>
2023-02-09 12:46:03 +0500

    claf.lua: fix require strings

--- lua/claf.lua
+++ lua/claf.lua
@@ -2,10 +2,10 @@
 --     include 'claf.lua'
 -- to the beginning of the file.
 
-table.Merge(_G, require('claf.aliases'))
-table.Merge(_G, require('claf.enums'))
-table.Merge(_G, require('claf.lib'))
-table.Merge(_G, require('claf.fmt'))
-table.Merge(_G, require('claf.functional'))
-table.Merge(_G, require('claf.pipe'))
-table.Merge(_G, require('claf.assertions'))
+table.Merge(_G, require('claf/aliases'))
+table.Merge(_G, require('claf/enums'))
+table.Merge(_G, require('claf/lib'))
+table.Merge(_G, require('claf/fmt'))
+table.Merge(_G, require('claf/functional'))
+table.Merge(_G, require('claf/pipe'))
+table.Merge(_G, require('claf/assertions'))
