d09e93a4b31563779a8fd4c5d6aa3b44b552fc46
javabird25 <shockck84@gmail.com>
2023-02-12 13:15:51 +0500

    Add test for Zip { { 1, 2, 3 } }

--- spec/functional/zip_spec.lua
+++ spec/functional/zip_spec.lua
@@ -2,23 +2,31 @@ require "spec/gmod_polyfills"
 local Zip = require("claf/functional").Zip
 
 describe("Zip", function()
-    it("zips multiple tables of equal lenghts", function()
+    it("returns input[1] if input is a table of length 1", function()
         local a = { 1, 2, 3 }
-        local b = { 4, 5, 6 }
-        local c = { 7, 8, 9 }
-
-        local zip = Zip { a, b, c }
-
-        assert.are.same({ { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } }, zip)
+        local zip = Zip { a }
+        assert.are.same(a, zip)
     end)
 
-    it("zips multiple tables of different lenghts", function()
-        local a = { 1, 2, 3 }
-        local b = { 4, 5, 6, 7 }
-        local c = { 8, 9, 10, 11, 12 }
+    describe("zips multiple tables of", function()
+        it("equal lenghts", function()
+            local a = { 1, 2, 3 }
+            local b = { 4, 5, 6 }
+            local c = { 7, 8, 9 }
 
-        local zip = Zip { a, b, c }
+            local zip = Zip { a, b, c }
 
-        assert.are.same({ { 1, 4, 8 }, { 2, 5, 9 }, { 3, 6, 10 } }, zip)
+            assert.are.same({ { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } }, zip)
+        end)
+
+        it("different lenghts", function()
+            local a = { 1, 2, 3 }
+            local b = { 4, 5, 6, 7 }
+            local c = { 8, 9, 10, 11, 12 }
+
+            local zip = Zip { a, b, c }
+
+            assert.are.same({ { 1, 4, 8 }, { 2, 5, 9 }, { 3, 6, 10 } }, zip)
+        end)
     end)
 end)
