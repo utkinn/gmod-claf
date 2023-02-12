69bbab9cf42d03f5e10d2a7f03e8767d96c90c2a
javabird25 <shockck84@gmail.com>
2023-02-12 11:37:32 +0500

    fmt refactoring

--- lua/claf/fmt.lua
+++ lua/claf/fmt.lua
@@ -1,75 +1,76 @@
 local mod = {}
 
-local f_deprecation_warns = 0
+local deprecationWarnsIssued = 0
 -- Deprecated
 function mod.f(str)
-    if f_deprecation_warns < 10 then
+    if deprecationWarnsIssued < 10 then
         MsgC(Color(255, 255, 0), "[CLAF] f() is deprecated. Use fmt() instead.")
-        f_deprecation_warns = f_deprecation_warns + 1
+        deprecationWarnsIssued = deprecationWarnsIssued + 1
     end
     return mod.fmt(str)
 end
 
--- TODO: refactor
-function mod.fmt(str)
-    -- ESCAPE_PLACEHOLDER_* are NOT empty. They actually contain "object replacement characters" (U+FFFC).
-    -- "{{" is replaced with one symbol, "}}" is replaced with two symbols.
-    -- U+FFFC was chosen because it wouldn't be used by anyone very likely.
-    local ESCAPE_PLACEHOLDER_BEGIN = "￼" -- U+FFFC, x1
-    local ESCAPE_PLACEHOLDER_END = "￼￼" -- U+FFFC, x2
+local function evalVarInCallerScope(name, stackFramesToGoUp)
+    for i = 1, math.huge do
+        local n, v = debug.getlocal(stackFramesToGoUp, i)
+        if n == nil then
+            -- Ran out of locals, give a global with this name a shot
+            return _G[name]
+        end
+        if n == name then
+            return v
+        end
+    end
+end
 
-    -- Replacing escaped curly brackets ("{{" and "}}") with escape placeholders.
+local function evaluateTableMember(expr)
+    local stackFramesToGoUp = 5
+
+    local exprParts = string.Explode(".", expr)
+    local value = evalVarInCallerScope(exprParts[1], stackFramesToGoUp)
+    table.remove(exprParts, 1)
+    for _, part in ipairs(exprParts) do
+        value = value[part]
+    end
+
+    return value
+end
+
+local function evaluatePattern(pattern)
+    local stackFramesToGoUp = 4
+
+    local patternWithoutBraces = string.sub(pattern, 2, -2)
+    local value
+    if string.match(patternWithoutBraces, "%.") then
+        value = evaluateTableMember(patternWithoutBraces)
+    else
+        value = evalVarInCallerScope(patternWithoutBraces, stackFramesToGoUp)
+    end
+
+    if value == nil then
+        value = "nil"
+    end
+
+    return value
+end
+
+function mod.fmt(str)
+    -- ESCAPE_PLACEHOLDER_* contain exotic Unicode characters from Private Use Area block
+    -- (https://en.wikipedia.org/wiki/Private_Use_Areas).
+    -- Since their purpose is not defined, they are not likely to be used in any input.
+    local ESCAPE_PLACEHOLDER_BEGIN = "" -- U+E000
+    local ESCAPE_PLACEHOLDER_END = "" -- U+E001
+
+    -- Lua patterns don't support lookbehind and lookahead, so as a workaround
+    -- we replace escaped brackets with a placeholder and then restore them
+    -- after the substitution.
     str = string.Replace(str, "{{", ESCAPE_PLACEHOLDER_BEGIN)
     str = string.Replace(str, "}}", ESCAPE_PLACEHOLDER_END)
 
-    -- Table of variable names to substitute
-    local variableNamesIter = string.gmatch(str, "{.-}")
-    local variableNames = {}
-    for v in variableNamesIter do
-        table.insert(variableNames, v)
+    for pattern in string.gmatch(str, "{.-}") do
+        str = string.Replace(str, pattern, evaluatePattern(pattern))
     end
 
-    -- Looking through this function caller's locals
-    local name, value
-    local locals = {}
-    local i = 1
-    while true do
-        name, value = debug.getlocal(2, i)
-        if name == nil then
-            break
-        end
-        locals[name] = value
-        i = i + 1
-    end
-
-    for _, varName in pairs(variableNames) do
-        local varNameWithoutBraces = string.sub(varName, 2, -2)
-        local value2
-        if string.match(varNameWithoutBraces, "%.") then
-            local varNameParts = string.Explode(".", varNameWithoutBraces)
-            if locals[varNameParts[1]] ~= nil then
-                value2 = locals[varNameParts[1]]
-            else
-                value2 = _G[varNameParts[1]]
-            end
-            table.remove(varNameParts, 1)
-            for _, part in pairs(varNameParts) do
-                value2 = value2[part]
-            end
-        elseif locals[varNameWithoutBraces] ~= nil then
-            value2 = locals[varNameWithoutBraces]
-        else
-            value2 = _G[varNameWithoutBraces]
-        end
-
-        -- Textual representation for nil
-        if value2 == nil then
-            value2 = "nil"
-        end
-        str = string.Replace(str, varName, value2)
-    end
-
-    -- Restoring escaped brackets
     str = string.Replace(str, ESCAPE_PLACEHOLDER_END, "}")
     str = string.Replace(str, ESCAPE_PLACEHOLDER_BEGIN, "{")
 
